fb86d1a2e5843973791726b02c257490b8ed8c71
Arjun <arjun.krishnakumar.mec@gmail.com>
2023-03-23 15:11:55 +0100

    feat: add code to play with nb201 search space

---
+++ examples/searchspace.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import torch
+
+from confopt.searchspace import NASBench201SearchSpace
+
+supernet = NASBench201SearchSpace()
+x = torch.randn(2, 3, 32, 32)
+
+compiled_model = torch.compile(supernet)  # PyTorch 2.0 only
+
+out, logits = supernet(x)
+
+print(out.shape)
+print(logits.shape)
+print('Done.')
