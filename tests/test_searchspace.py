41d5e377082c4b58d4eafce8273f7ab55129f23f
Arjun <arjun.krishnakumar.mec@gmail.com>
2023-03-27 21:29:55 +0200

    test(searchspace): add tests for DARTS search space

--- tests/test_searchspace.py
+++ tests/test_searchspace.py
@@ -3,7 +3,7 @@ import unittest
 import torch
 import torch.nn as nn  # noqa: PLR0402
 
-from confopt.searchspace import NASBench201SearchSpace
+from confopt.searchspace import DARTSSearchSpace, NASBench201SearchSpace
 
 
 class TestNASBench201SearchSpace(unittest.TestCase):
@@ -25,3 +25,26 @@ class TestNASBench201SearchSpace(unittest.TestCase):
         assert isinstance(out[1], torch.Tensor)
         assert out[0].shape == torch.Size([2, 64])
         assert out[1].shape == torch.Size([2, 10])
+
+class TestDARTSSearchSpace(unittest.TestCase):
+    def test_arch_parameters(self) -> None:
+        search_space = DARTSSearchSpace()
+        arch_params = search_space.arch_parameters
+        assert len(arch_params) >= 1
+        assert isinstance(arch_params[0], nn.Parameter)
+
+    def test_forward_pass(self) -> None:
+        search_space = DARTSSearchSpace()
+        x = torch.randn(2, 3, 64, 64)
+
+        out = search_space(x)
+
+        assert isinstance(out, tuple)
+        assert len(out) == 2
+        assert isinstance(out[0], torch.Tensor)
+        assert isinstance(out[1], torch.Tensor)
+        assert out[0].shape == torch.Size([2, 256])
+        assert out[1].shape == torch.Size([2, 10])
+
+if __name__ == "__main__":
+    unittest.main()
