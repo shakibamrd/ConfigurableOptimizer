9e355e0a60fcd057d323019580a9d3e5dc067be1
Arjun <arjun.krishnakumar.mec@gmail.com>
2023-03-27 20:21:00 +0200

    test: add test for nb201 search space

---
+++ tests/test_searchspace.py
@@ -0,0 +1,27 @@
+import unittest
+
+import torch
+import torch.nn as nn  # noqa: PLR0402
+
+from confopt.searchspace import NASBench201SearchSpace
+
+
+class TestNASBench201SearchSpace(unittest.TestCase):
+    def test_arch_parameters(self) -> None:
+        search_space = NASBench201SearchSpace()
+        arch_params = search_space.arch_parameters
+        assert len(arch_params) >= 1
+        assert isinstance(arch_params[0], nn.Parameter)
+
+    def test_forward_pass(self) -> None:
+        search_space = NASBench201SearchSpace()
+        x = torch.randn(2, 3, 32, 32)
+
+        out = search_space(x)
+
+        assert isinstance(out, tuple)
+        assert len(out) == 2
+        assert isinstance(out[0], torch.Tensor)
+        assert isinstance(out[1], torch.Tensor)
+        assert out[0].shape == torch.Size([2, 64])
+        assert out[1].shape == torch.Size([2, 10])
