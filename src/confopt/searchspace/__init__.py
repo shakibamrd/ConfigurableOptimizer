e26ddd8aea94ac6b8e9f97600d06ea26cfd301f4
abhash-er <>
2023-04-19 01:53:00 +0200

    feat(searchspace): expose nb1shot1 searchspace api

--- src/confopt/searchspace/__init__.py
+++ src/confopt/searchspace/__init__.py
@@ -1,4 +1,5 @@
 from .darts.supernet import DARTSSearchSpace  # type: ignore
+from .nb1shot1.supernet import NASBench1Shot1SearchSpace  # type: ignore
 from .nb201.supernet import NASBench201SearchSpace  # type: ignore
 
-__all__ = ["NASBench201SearchSpace", "DARTSSearchSpace"]
+__all__ = ["NASBench201SearchSpace", "DARTSSearchSpace", "NASBench1Shot1SearchSpace"]
