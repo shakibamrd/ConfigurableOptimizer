157ff151dea256f432e99f11dcf18a8f43579d64
gierle <leon.gieringer@gmx.de>
2023-04-24 15:34:11 +0200

    feat: add SNAS architecture sampler

--- src/confopt/oneshot/archsampler/__init__.py
+++ src/confopt/oneshot/archsampler/__init__.py
@@ -2,5 +2,6 @@ from .base_sampler import BaseSampler
 from .darts.sampler import DARTSSampler
 from .drnas.sampler import DRNASSampler
 from .gdas.sampler import GDASSampler
+from .snas.sampler import SNASSampler
 
-__all__ = ["BaseSampler", "DARTSSampler", "DRNASSampler", "GDASSampler"]
+__all__ = ["BaseSampler", "DARTSSampler", "DRNASSampler", "GDASSampler", "SNASSampler"]
