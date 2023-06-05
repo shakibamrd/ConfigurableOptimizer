7cf6d7b3d65cc032c2befd2d856599c5be436918
abhash-er <>
2023-06-05 20:50:34 +0200

    feat(oneshot): exposed sdarts sampler api

--- src/confopt/oneshot/archsampler/__init__.py
+++ src/confopt/oneshot/archsampler/__init__.py
@@ -2,6 +2,14 @@ from .base_sampler import BaseSampler
 from .darts.sampler import DARTSSampler
 from .drnas.sampler import DRNASSampler
 from .gdas.sampler import GDASSampler
+from .sdarts.sampler import SDARTSSampler
 from .snas.sampler import SNASSampler
 
-__all__ = ["BaseSampler", "DARTSSampler", "DRNASSampler", "GDASSampler", "SNASSampler"]
+__all__ = [
+    "BaseSampler",
+    "DARTSSampler",
+    "DRNASSampler",
+    "GDASSampler",
+    "SNASSampler",
+    "SDARTSSampler",
+]
