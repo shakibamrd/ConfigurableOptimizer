2ef78d484319d7482233be7cfd83a03841aee072
Arjun <arjun.krishnakumar.mec@gmail.com>
2023-06-27 12:24:25 +0200

    feat: add first iteration of Trainer and Experiment classes

--- src/confopt/searchspace/__init__.py
+++ src/confopt/searchspace/__init__.py
@@ -1,5 +1,13 @@
+from .adapt import adapt_search_space
+from .common.base_search import SearchSpace
 from .darts.supernet import DARTSSearchSpace  # type: ignore
 from .nb1shot1.supernet import NASBench1Shot1SearchSpace  # type: ignore
 from .nb201.supernet import NASBench201SearchSpace  # type: ignore
 
-__all__ = ["NASBench201SearchSpace", "DARTSSearchSpace", "NASBench1Shot1SearchSpace"]
+__all__ = [
+    "NASBench201SearchSpace",
+    "DARTSSearchSpace",
+    "NASBench1Shot1SearchSpace",
+    "adapt_search_space",
+    "SearchSpace",
+]
