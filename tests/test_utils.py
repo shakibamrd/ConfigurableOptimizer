2cadba6c8cf31865cbc8fb77ba57d30916105b1c
shakibamrd <shakiba.moradian@yahoo.com>
2023-07-27 13:43:36 +0200

    test(partial_connector): add tests for partial_connector

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,10 @@ from pathlib import Path
 import shutil
 import unittest
 
+import torch
+
 from confopt.utils import prepare_logger
+from confopt.utils.channel_shuffle import channel_shuffle
 
 
 class TestUtils(unittest.TestCase):
@@ -16,6 +19,14 @@ class TestUtils(unittest.TestCase):
         assert os.path.exists(logger.logger_path)
 
         shutil.rmtree(save_dir)
+
+    def test_channel_shuffle(self) -> None:
+        k = 4
+        x = torch.randn(2, 8, 3, 5)
+        original_x = x.detach().clone()
+        shuffled_x = channel_shuffle(x, groups=k)
+        assert x.shape == shuffled_x.shape
+        assert not torch.allclose(original_x, shuffled_x)
 
 
 if __name__ == "__main__":
