9f0840f5f36a9ca5591c5f780d55fe981b74a380
abhash-er <>
2023-08-30 17:27:06 +0200

    feat(experiment): added a example runner for darts search

---
+++ scripts/darts_runner.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from torch import nn, optim
+import wandb
+
+from confopt.dataset import CIFAR10Data
+from confopt.oneshot.archsampler import DARTSSampler
+from confopt.searchspace import DARTSSearchSpace
+from confopt.train import ConfigurableTrainer
+from confopt.utils import BaseProfile, prepare_logger
+
+
+def get_hyperparameters() -> dict:
+    # This is just for test
+    # TODO build a yaml config file for each of the config
+    # Change the parameters here for now
+    hyperparameters = {
+        "model_lr": 1e-3,
+        "arch_lr": 1e-3,
+        "model_momentum": 0.0,
+        "arch_betas": (0.9, 0.99),
+        "weight_decay": 0.1,
+        "epochs": 1,
+        "dataset": "CIFAR10",
+        "exp_name": "DARTS",
+    }
+    return hyperparameters
+
+# Tie logger and wandb together
+logger = prepare_logger(
+            save_dir="logs",
+            seed=0,
+            exp_name="DARTS"
+        )
+
+
+def run_experiment() -> None:
+    wandb.init(                            # type: ignore
+        project="Configurable_Optimizers",
+        name="DARTS",
+        config=get_hyperparameters()
+        )
+
+    config = wandb.config # type: ignore
+    data = CIFAR10Data("datasets", 0, 0.5)
+    search_space = DARTSSearchSpace()
+    samplers = [DARTSSampler(search_space.arch_parameters)]
+    model_optimizer = optim.SGD(search_space.arch_parameters,
+                            lr=config["model_lr"],
+                            momentum=config["model_momentum"],
+                            weight_decay=config["weight_decay"]
+                        )
+    arch_optimizer = optim.Adam(search_space.arch_parameters,
+                                lr=config["arch_lr"],
+                                betas=config["arch_betas"],
+                                weight_decay=0.1
+                            )
+    lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(model_optimizer,
+                                                        T_max=config["epochs"]
+                                                    )
+    criterion = nn.CrossEntropyLoss()
+    profile = BaseProfile(samplers)
+
+    trainer = ConfigurableTrainer(
+                                model=search_space,
+                                data=data,
+                                model_optimizer=model_optimizer,
+                                arch_optimizer=arch_optimizer,
+                                scheduler=lr_scheduler,
+                                criterion=criterion,
+                                batchsize=16,
+                                logger=logger
+                            )
+    trainer.train(profile, epochs=config["epochs"], is_wandb_log=True)
+    logger.close()
+    wandb.finish() # type: ignore
+
+
+if __name__ == "__main__":
+    run_experiment()
