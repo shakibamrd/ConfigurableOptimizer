8bd8dd06e3ea9b15eef644427cfa0b44caba4068
abhash-er <>
2023-09-19 12:38:02 +0200

    fix(OperationBlock): changed OperationBlock following the changes in PartialConnector

--- src/confopt/searchspace/common/mixop.py
+++ src/confopt/searchspace/common/mixop.py
@@ -6,6 +6,7 @@ from torch import nn
 from confopt.oneshot.archsampler import BaseSampler
 from confopt.oneshot.partial_connector import PartialConnector
 
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 __all__ = ["OperationChoices"]
 
 
@@ -23,26 +24,37 @@ class OperationChoices(nn.Module):
         return sum(states)  # type: ignore
 
 
-class OperationBlock:
+class OperationBlock(nn.Module):
     def __init__(
         self,
         ops: list[nn.Module],
         is_reduction_cell: bool,
         arch_sampler: BaseSampler,
+        arch_modifier: BaseSampler,
         partial_connector: PartialConnector = None,
+        device: torch.device = DEVICE,
     ) -> None:
+        super().__init__()
+        if partial_connector:
+            for op in ops:
+                op.change_channel_size(partial_connector.k, device)  # type: ignore
         self.ops = ops
         self.is_reduction_cell = is_reduction_cell
         self.arch_sampler = arch_sampler
+        self.arch_modifier = arch_modifier
         self.partial_connector = partial_connector
 
     def forward(self, x: torch.Tensor, alphas: list[torch.Tensor]) -> torch.Tensor:
         alphas_index = 1 if self.is_reduction_cell else 0
 
-        alphas = self.arch_sampler.sampled_alphas[alphas_index]
+        if self.arch_sampler is not None:
+            alphas = self.arch_sampler.sampled_alphas[alphas_index]
+
+        if self.arch_modifier is not None:
+            alphas = self.arch_modifier.sampled_alphas[alphas_index]
 
         if self.partial_connector:
-            return self.partial_connector(x, alphas)
+            return self.partial_connector(x, alphas, self.ops)
 
         states = [op(x) * alpha for op, alpha in zip(self.ops, alphas)]
 
