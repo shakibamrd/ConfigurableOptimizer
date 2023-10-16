888c5a6dad6e7a2c17b6c2f628d24bfaba57adbf
abhash-er <>
2023-10-16 13:41:44 +0200

    feat(ProfilConfig): add snas and drnas into the default profiles

--- src/confopt/profiles/profiles.py
+++ src/confopt/profiles/profiles.py
@@ -31,3 +31,41 @@ class GDASProfile(ProfileConfig):
     def get_sampler_config(self) -> dict:
         gdas_config = {"sample_frequency": "epoch", "tau_min": 0.1, "tau_max": 10}
         return gdas_config
+
+
+class SNASProfile(ProfileConfig):
+    def __init__(
+        self, is_partial_connection: bool = False, perturbation: str | None = None
+    ) -> None:
+        PROFILE_TYPE = "SNAS"
+        super().__init__(PROFILE_TYPE)
+        self.sampler_type = PROFILE_TYPE
+        self.set_partial_connector(is_partial_connection)
+        self.set_perturb(perturbation)
+
+    def get_sampler_config(self) -> dict:
+        snas_config = {
+            "sample_frequency": "epoch",
+            "temp_init": 1.0,
+            "temp_min": 0.33,
+            "temp_annealing": True,
+            "total_epochs": 250,
+        }
+        return snas_config
+
+
+class DRNAS(ProfileConfig):
+    def __init__(
+        self, is_partial_connection: bool = False, perturbation: str | None = None
+    ) -> None:
+        PROFILE_TYPE = "DRNAS"
+        super().__init__(PROFILE_TYPE)
+        self.sampler_type = PROFILE_TYPE
+        self.set_partial_connector(is_partial_connection)
+        self.set_perturb(perturbation)
+
+    def get_sampler_config(self) -> dict:
+        drnas_config = {
+            "sample_frequency": "epoch",
+        }
+        return drnas_config
