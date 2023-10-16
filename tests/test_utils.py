6d0dc17b4b9d1d54c35638fdfd7c87f563c69ca9
abhash-er <>
2023-10-16 14:02:23 +0200

    test(test_utils): fix the test for logger

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,8 +14,8 @@ class TestUtils(unittest.TestCase):
         save_dir = Path(".") / "tests" / "temp"
         exp_name = "test_exp"
         logger = prepare_logger(save_dir=save_dir, seed=9001, exp_name=exp_name)
-        assert logger.log_dir == save_dir / exp_name
-        assert os.path.exists(save_dir / "checkpoint" / exp_name)
+        checkpoints_path = logger.path(mode="checkpoints")
+        assert os.path.exists(checkpoints_path)
         assert os.path.exists(logger.logger_path)
 
         shutil.rmtree(save_dir, ignore_errors=True)
