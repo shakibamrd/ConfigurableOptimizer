5db46c940d3b20585af348590320c1b9ceec08a4
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2023-10-25 17:40:04 +0200

    fix(example): fix bug in search space example
    The tensor was not being loaded onto cuda when it is available

--- examples/searchspace.py
+++ examples/searchspace.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import torch
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 from confopt.searchspace import NASBench201SearchSpace
 
 supernet = NASBench201SearchSpace()
-x = torch.randn(2, 3, 32, 32)
+x = torch.randn(2, 3, 32, 32).to(device)
 
 compiled_model = torch.compile(supernet)  # PyTorch 2.0 only
 
