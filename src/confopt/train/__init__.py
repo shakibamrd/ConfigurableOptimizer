0a35f77d48b1f65cba8d140cf7fecef6ec703181
abhash-er <abhashk2@outlook.com>
2023-11-08 18:16:20 +0100

    feat(confopt.train): add enums and experiment class to be used by user
    add `Experiment`,`SearchSpaceType`,`DatasetType`,`SamplerType`,`PerturbatorType`

--- src/confopt/train/__init__.py
+++ src/confopt/train/__init__.py
@@ -1,4 +1,19 @@
 from .configurable_trainer import ConfigurableTrainer
+from .experiment import (
+    DatasetType,
+    Experiment,
+    PerturbatorType,
+    SamplerType,
+    SearchSpaceType,
+)
 from .searchprofile import Profile
 
-__all__ = ["ConfigurableTrainer", "Profile"]
+__all__ = [
+    "ConfigurableTrainer",
+    "Profile",
+    "Experiment",
+    "SearchSpaceType",
+    "DatasetType",
+    "SamplerType",
+    "PerturbatorType",
+]
