e99da6da9c32246c719c4a816e77b287314affbc
abhash-er <abhashk2@outlook.com>
2023-11-12 09:35:08 +0100

    feat(random_search): add default run and experiment group parameter for random search script

--- scripts/random_search.py
+++ scripts/random_search.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import argparse
 import traceback
 
+from ConfigSpace import Configuration
 import torch
 import wandb
 
@@ -10,13 +11,59 @@ from confopt.profiles import ConfigSpaceProfile
 from confopt.train import (
     DatasetType,
     Experiment,
-    PerturbatorType,
-    SamplerType,
     SearchSpaceType,
 )
 from confopt.utils import get_configspace
 
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+# manage wandb run_names
+run_names: dict = {}
+
+
+def get_experiment(
+    cs: Configuration, mode: str = "random"
+) -> tuple[ConfigSpaceProfile, Experiment]:
+    assert mode in ["random", "default"]
+
+    if mode == "default":
+        config = cs.get_default_configuration()
+    else:
+        config = cs.sample_configuration()
+
+    cs_profile = ConfigSpaceProfile(
+        config, epochs=args.epochs, run_group=args.experiment_group  # type: ignore
+    )
+
+    searchspace = SearchSpaceType(args.searchspace)  # type: ignore
+    dataset = DatasetType(args.dataset)  # type: ignore
+
+    if mode == "default":
+        run_name = "default_run"
+    else:
+        run_name = args.searchspace + "_" + config["sampler"]
+        if config["perturbator"] != "none":
+            run_name += "_" + config["perturbator"]
+        if config["is_partial_connector"]:
+            run_name += "_" + "pc"
+
+    if args.wandb_log:  # type: ignore
+        run_num = 0
+        if run_name not in run_names:
+            run_names[run_name] = run_num
+        else:
+            run_names[run_name] += 1
+            run_num = run_names[run_name]
+        run_name += "_" + str(run_num)
+
+    experiment = Experiment(
+        search_space=searchspace,
+        dataset=dataset,
+        seed=args.seed,  # type: ignore
+        is_wandb_log=args.wandb_log,  # type: ignore
+        exp_name=run_name,
+    )
+    return cs_profile, experiment
 
 
 if __name__ == "__main__":
@@ -69,46 +116,35 @@ if __name__ == "__main__":
         default=False,
         help="log on wandb",
     )
+    parser.add_argument(
+        "--experiment_group",
+        default="random_search",
+        help="group experiments uniquely on wandb",
+        type=str,
+    )
     args = parser.parse_args()
 
     cs = get_configspace(seed=args.seed)
 
-    # manage wandb runs
-    run_names = {}
+    # default run
+    cs_profile, experiment = get_experiment(cs, mode="default")
+    try:
+        experiment.run_with_profile(
+            cs_profile,
+        )
+        if args.wandb_log:  # type: ignore
+            wandb.finish()  # type: ignore
+        experiment.logger.close()
+    except Exception as e:  # noqa: BLE001
+        print(e)
+        traceback.print_exc()
+        if args.wandb_log:  # type: ignore
+            print("Check wandb logs to see where the search crashed")
+            wandb.finish(1)  # type: ignore
+        experiment.logger.close()
 
     for _ in range(args.num_exp):  # type: ignore
-        config = cs.sample_configuration()
-        cs_profile = ConfigSpaceProfile(config, epochs=5)
-        searchspace = SearchSpaceType(args.searchspace)  # type: ignore
-        sampler = SamplerType(config["sampler"])
-        perturbator = PerturbatorType(config.get("perturbator", None))
-        dataset = DatasetType(args.dataset)  # type: ignore
-        edge_normalization = config.get("edge_normalization", False)
-        is_partial_connection = config["is_partial_connector"]
-
-        run_name = args.searchspace + "_" + config["sampler"]
-        if config["perturbator"] != "none":
-            run_name += "_" + config["perturbator"]
-        if config["is_partial_connector"]:
-            run_name += "_" + "pc"
-
-        if args.wandb_log:  # type: ignore
-            run_num = 0
-            if run_name not in run_names:
-                run_names[run_name] = run_num
-            else:
-                run_names[run_name] += 1
-                run_num = run_names[run_name]
-            run_name += "_" + str(run_num)
-
-        experiment = Experiment(
-            search_space=searchspace,
-            dataset=dataset,
-            seed=args.seed,  # type: ignore
-            is_wandb_log=args.wandb_log,  # type: ignore
-            exp_name=run_name,
-            debug_mode=True,
-        )
+        cs_profile, experiment = get_experiment(cs)
         try:
             experiment.run_with_profile(
                 cs_profile,
