069517321adb0e07f0d8c3143454970f8c281fa0
abhash-er <abhashk2@outlook.com>
2023-12-13 17:23:06 +0100

    fix(BaseSampler): remove set_arch_from_sample function to update to new design
    the settting of arch param could not be mapped to compuational graph

--- src/confopt/oneshot/archsampler/base_sampler.py
+++ src/confopt/oneshot/archsampler/base_sampler.py
@@ -16,7 +16,7 @@ class BaseSampler(OneShotComponent):
     ) -> None:
         super().__init__()
         self.arch_parameters = arch_parameters
-        self.sampled_alphas: list[torch.Tensor] = arch_parameters
+        self.sampled_alphas: list[torch.Tensor] | None = None
 
         assert sample_frequency in [
             "epoch",
@@ -30,10 +30,6 @@ class BaseSampler(OneShotComponent):
     ) -> list[torch.Tensor] | None:
         pass
 
-    def set_arch_parameters_from_sample(self) -> None:
-        assert self.sampled_alphas is not None
-        self.arch_parameters = self.sampled_alphas
-
     def _sample_and_update_alphas(self) -> None:  # type: ignore
         sampled_alphas = self.sample_alphas(self.arch_parameters)
         # print(sampled_alphas)
@@ -44,11 +40,8 @@ class BaseSampler(OneShotComponent):
         super().new_epoch()
         if self.sample_frequency == "epoch":
             self._sample_and_update_alphas()
-            self.set_arch_parameters_from_sample()
 
     def new_step(self) -> None:  # type: ignore
         super().new_step()
-
         if self.sample_frequency == "step":
             self._sample_and_update_alphas()
-            self.set_arch_parameters_from_sample()
