7375fadc86481f8e5cde7a3f97da6d5621925650
abhash-er <abhashk2@outlook.com>
2023-12-22 13:29:36 +0100

    test(test_profiles): tests for initialization and configuring profiles

---
+++ tests/test_profiles.py
@@ -0,0 +1,256 @@
+import unittest
+
+from confopt.profiles import (
+    DartsProfile,
+    DRNASProfile,
+    GDASProfile,
+    ProfileConfig,
+    SNASProfile,
+)
+
+
+class TestProfileConfig(unittest.TestCase):
+    def test_config_change(self) -> None:
+        profile = ProfileConfig(
+            "TEST",
+            is_partial_connection=True,
+            perturbation="random",
+            perturbator_sample_frequency="step",
+        )
+
+        partial_connector_config = {"k": 2}
+
+        perturbator_config = {
+            "epsilon": 0.5,
+        }
+
+        trainer_config = {"use_data_parallel": True}
+
+        profile.configure_partial_connector(**partial_connector_config)
+        profile.configure_perturbator(**perturbator_config)
+        profile.configure_trainer(**trainer_config)
+
+        assert profile.partial_connector_config["k"] == partial_connector_config["k"]
+        assert profile.perturb_config["epsilon"] == perturbator_config["epsilon"]
+        assert (
+            profile.trainer_config["use_data_parallel"]
+            == trainer_config["use_data_parallel"]
+        )
+
+    def test_invalid_configuration(self) -> None:
+        profile = ProfileConfig(
+            "TEST",
+            is_partial_connection=True,
+            perturbation="random",
+            perturbator_sample_frequency="step",
+        )
+
+        partial_connector_config = {"invalid_config": 2}
+
+        perturbator_config = {
+            "invalid_config": 0.5,
+        }
+
+        trainer_config = {"invalid_config": False}
+
+        with self.assertRaises(AssertionError):
+            profile.configure_partial_connector(**partial_connector_config)
+
+        with self.assertRaises(AssertionError):
+            profile.configure_perturbator(**perturbator_config)
+
+        with self.assertRaises(AssertionError):
+            profile.configure_trainer(**trainer_config)
+
+
+class TestDartsProfile(unittest.TestCase):
+    def test_initialization(self) -> None:
+        perturb_config = {"epsilon": 0.5}
+        partial_connector_config = {
+            "k": 2,
+        }
+        profile = DartsProfile(
+            is_partial_connection=True,
+            perturbation="random",
+            sampler_sample_frequency="step",
+            partial_connector_config=partial_connector_config,
+            perturbator_config=perturb_config,
+        )
+
+        assert profile.sampler_config is not None
+        assert profile.partial_connector_config["k"] == partial_connector_config["k"]
+        assert profile.perturb_config["epsilon"] == perturb_config["epsilon"]
+
+    def test_invalid_initialization(self) -> None:
+        perturb_config = {"invalid_config": 0.5}
+        partial_connector_config = {
+            "invalid_config": 2,
+        }
+
+        with self.assertRaises(AssertionError):
+            profile = DartsProfile(  # noqa: F841
+                is_partial_connection=True,
+                perturbation="random",
+                sampler_sample_frequency="step",
+                partial_connector_config=partial_connector_config,
+                perturbator_config=perturb_config,
+            )
+
+    def test_sampler_change(self) -> None:
+        profile = DartsProfile(
+            sampler_sample_frequency="step",
+        )
+        sampler_config = {"sample_frequency": "epoch"}
+        profile.configure_sampler(**sampler_config)
+        assert (
+            profile.sampler_config["sample_frequency"]
+            == sampler_config["sample_frequency"]
+        )
+
+        with self.assertRaises(AssertionError):
+            profile.configure_sampler(invalid_config="step")
+
+
+class TestDRNASProfile(unittest.TestCase):
+    def test_initialization(self) -> None:
+        perturb_config = {"epsilon": 0.5}
+        partial_connector_config = {
+            "k": 2,
+        }
+        profile = DRNASProfile(
+            is_partial_connection=True,
+            perturbation="random",
+            sampler_sample_frequency="step",
+            partial_connector_config=partial_connector_config,
+            perturbator_config=perturb_config,
+        )
+
+        assert profile.sampler_config is not None
+        assert profile.partial_connector_config["k"] == partial_connector_config["k"]
+        assert profile.perturb_config["epsilon"] == perturb_config["epsilon"]
+
+    def test_invalid_initialization(self) -> None:
+        perturb_config = {"invalid_config": 0.5}
+        partial_connector_config = {
+            "invalid_config": 2,
+        }
+
+        with self.assertRaises(AssertionError):
+            profile = DRNASProfile(  # noqa: F841
+                is_partial_connection=True,
+                perturbation="random",
+                sampler_sample_frequency="step",
+                partial_connector_config=partial_connector_config,
+                perturbator_config=perturb_config,
+            )
+
+    def test_sampler_change(self) -> None:
+        profile = DRNASProfile(
+            sampler_sample_frequency="step",
+        )
+        sampler_config = {"sample_frequency": "epoch"}
+        profile.configure_sampler(**sampler_config)
+        assert (
+            profile.sampler_config["sample_frequency"]
+            == sampler_config["sample_frequency"]
+        )
+
+        with self.assertRaises(AssertionError):
+            profile.configure_sampler(invalid_config="step")
+
+
+class TestGDASProfile(unittest.TestCase):
+    def test_initialization(self) -> None:
+        perturb_config = {"epsilon": 0.5}
+        partial_connector_config = {
+            "k": 2,
+        }
+        profile = GDASProfile(
+            is_partial_connection=True,
+            perturbation="random",
+            sampler_sample_frequency="step",
+            partial_connector_config=partial_connector_config,
+            perturbator_config=perturb_config,
+        )
+
+        assert profile.sampler_config is not None
+        assert profile.partial_connector_config["k"] == partial_connector_config["k"]
+        assert profile.perturb_config["epsilon"] == perturb_config["epsilon"]
+
+    def test_invalid_initialization(self) -> None:
+        perturb_config = {"invalid_config": 0.5}
+        partial_connector_config = {
+            "invalid_config": 2,
+        }
+
+        with self.assertRaises(AssertionError):
+            profile = GDASProfile(  # noqa: F841
+                is_partial_connection=True,
+                perturbation="random",
+                sampler_sample_frequency="step",
+                partial_connector_config=partial_connector_config,
+                perturbator_config=perturb_config,
+            )
+
+    def test_sampler_change(self) -> None:
+        profile = GDASProfile(
+            sampler_sample_frequency="step",
+        )
+        sampler_config = {"tau_max": 12, "tau_min": 0.3}
+        profile.configure_sampler(**sampler_config)
+
+        assert profile.sampler_config["tau_max"] == sampler_config["tau_max"]
+        assert profile.sampler_config["tau_min"] == sampler_config["tau_min"]
+
+        with self.assertRaises(AssertionError):
+            profile.configure_sampler(invalid_config="step")
+
+
+class TestSNASProfile(unittest.TestCase):
+    def test_initialization(self) -> None:
+        perturb_config = {"epsilon": 0.5}
+        partial_connector_config = {
+            "k": 2,
+        }
+        profile = SNASProfile(
+            is_partial_connection=True,
+            perturbation="random",
+            sampler_sample_frequency="step",
+            partial_connector_config=partial_connector_config,
+            perturbator_config=perturb_config,
+        )
+
+        assert profile.sampler_config is not None
+        assert profile.partial_connector_config["k"] == partial_connector_config["k"]
+        assert profile.perturb_config["epsilon"] == perturb_config["epsilon"]
+
+    def test_invalid_initialization(self) -> None:
+        perturb_config = {"invalid_config": 0.5}
+        partial_connector_config = {
+            "invalid_config": 2,
+        }
+
+        with self.assertRaises(AssertionError):
+            profile = SNASProfile(  # noqa: F841
+                is_partial_connection=True,
+                perturbation="random",
+                sampler_sample_frequency="step",
+                partial_connector_config=partial_connector_config,
+                perturbator_config=perturb_config,
+            )
+
+    def test_sampler_change(self) -> None:
+        profile = SNASProfile(
+            sampler_sample_frequency="step",
+        )
+        sampler_config = {"temp_min": 0.5, "temp_init": 1.3}
+        profile.configure_sampler(**sampler_config)
+        assert profile.sampler_config["temp_min"] == sampler_config["temp_min"]
+        assert profile.sampler_config["temp_init"] == sampler_config["temp_init"]
+
+        with self.assertRaises(AssertionError):
+            profile.configure_sampler(invalid_config="step")
+
+
+if __name__ == "__main__":
+    unittest.main()
