061ffc97c4fbc7e1522412b2082f1b50d39733cf
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-05-23 16:54:03 +0200

    chore: set up experiment for 300 epochs alternating

--- scripts/jobs/run_all_lora_experiments.sh
+++ scripts/jobs/run_all_lora_experiments.sh
@@ -9,7 +9,7 @@ for space in "${spaces[@]}"; do
     for sampler in "${samplers[@]}"; do
         for entanglement in "${we[@]}"; do
             echo scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement
-            sbatch -J LoRA-r${rank}-${sampler}-${space}-WE-${entanglement}-300epochs scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement $rank
+            sbatch -J LoRA-r${rank}-${sampler}-${space}-WE-${entanglement}-alternate-1epoch-300epochs scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement $rank
         done
     done
 done
