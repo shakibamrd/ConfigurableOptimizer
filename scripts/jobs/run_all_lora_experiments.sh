80bec3272030c9f4a8dd7273a5f71bdf29ffb4ee
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-05-23 15:28:21 +0200

    chore: set up lora rank 1 experiments

--- scripts/jobs/run_all_lora_experiments.sh
+++ scripts/jobs/run_all_lora_experiments.sh
@@ -3,12 +3,13 @@
 spaces=("darts" "nb201")
 samplers=("darts" "drnas")
 we=("true" "false")
+rank=1
 
 for space in "${spaces[@]}"; do
     for sampler in "${samplers[@]}"; do
         for entanglement in "${we[@]}"; do
             echo scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement
-            sbatch -J LoRA-${sampler}-${space}-WE-${entanglement} scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement
+            sbatch -J LoRA-r${rank}-${sampler}-${space}-WE-${entanglement} scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement $rank
         done
     done
 done
