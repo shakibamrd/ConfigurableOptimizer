ebba1dbcda8d6b2805cee0d65b07c9975efbbccb
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-05-30 23:32:40 +0200

    chore: set up darts ws alternate experiment

--- scripts/jobs/run_all_lora_experiments.sh
+++ scripts/jobs/run_all_lora_experiments.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 spaces=("darts")
-samplers=("darts" "drnas")
-we=("false" "true")
+samplers=("darts")
+we=("false")
 rank=1
 
 for space in "${spaces[@]}"; do
     for sampler in "${samplers[@]}"; do
         for entanglement in "${we[@]}"; do
             echo scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement
-            sbatch -J LoRA-${sampler}-${space}-WE-${entanglement}-100epochs scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement $rank
+            sbatch -J LoRA-alternate-${sampler}-${space}-WE-${entanglement}-100epochs scripts/jobs/submit_lora_experiment.sh $space $sampler $entanglement $rank
         done
     done
 done
