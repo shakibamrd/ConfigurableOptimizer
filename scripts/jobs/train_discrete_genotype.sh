87f570749e48c864d70fbb56fe47f36b7eb45250
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-06-01 21:48:35 +0200

    fix: fix seeding in training genotypes

--- scripts/jobs/train_discrete_genotype.sh
+++ scripts/jobs/train_discrete_genotype.sh
@@ -2,8 +2,8 @@
 #SBATCH -p ml_gpu-rtx2080 #alldlc_gpu-rtx2080 #relea_gpu-rtx2080 #mlhiwidlc_gpu-rtx2080 # partition (queue)
 #SBATCH -o logs/%x.%N.%j.out # STDOUT
 #SBATCH -e logs/%x.%N.%j.err # STDERR
-#SBATCH -a 1-3 # array size
-#SBATCH --time 3-00:00:00 # time (D-HH:MM)
+#SBATCH -a 1-2 # array size
+#SBATCH --time 4-00:00:00 # time (D-HH:MM)
 #SBATCH --cpus-per-task 2
 #SBATCH -J TRAIN_GENOTYPE # sets the job name.
 echo "Workingdir: $PWD";
@@ -33,5 +33,4 @@ conda activate confopt
 
 # export WANDB_MODE="offline"
 # echo "$genotype"
-python scripts/train_discrete/train_genotype.py --wandb_log --searchspace "$searchspace" --genotype "$genotype"  --run_name "$run_name" #--seed $SLURM_ARRAY_TASK_ID
-
+python scripts/train_discrete/train_genotype.py --wandb_log --searchspace "$searchspace" --genotype "$genotype"  --run_name "$run_name" --seed $SLURM_ARRAY_TASK_ID
