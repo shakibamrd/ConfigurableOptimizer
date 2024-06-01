8b524028062f3dd4854da7c916be17771ade79a4
abhash-er <abhashk2@outlook.com>
2024-06-01 11:07:29 +0200

    chore: update script for lora alternating epoch

--- scripts/jobs/submit_train_genotype_jobs.sh
+++ scripts/jobs/submit_train_genotype_jobs.sh
@@ -4,17 +4,23 @@ searchspace="darts"
 
 # write a list of genotypes to train
 # change run name
-run_names=(
-    "darts_we_vanilla"
-    "darts_ws_vanilla"
-    "drnas_we_vanilla"
-    "drnas_ws_vanilla"
-    "darts_we_lora_rank_1"
-    "darts_ws_lora_rank_1"
-    "drnas_we_lora_rank_1"
-    "drnas_ws_lora_rank_1"
-)
+# run_names=(
+#     "darts_we_vanilla"
+#     "darts_ws_vanilla"
+#     "drnas_we_vanilla"
+#     "drnas_ws_vanilla"
+#     "darts_we_lora_rank_1"
+#     "darts_ws_lora_rank_1"
+#     "drnas_we_lora_rank_1"
+#     "drnas_ws_lora_rank_1"
+# )
 
+run_names=(
+  "darts_we_lora_alt"
+  "darts_ws_lora_alt"
+  "drnas_we_lora_alt"
+  "drnas_ws_lora_alt"
+)
 
 # Iterate over the lists and call the existing script
 for i in "${!run_names[@]}"; do
