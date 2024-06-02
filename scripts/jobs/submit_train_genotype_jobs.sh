156504a1d149aa060f3f77a20b552feff9535fb7
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-06-02 11:37:10 +0200

    chore: set up DARTS WE genotype training

--- scripts/jobs/submit_train_genotype_jobs.sh
+++ scripts/jobs/submit_train_genotype_jobs.sh
@@ -17,17 +17,17 @@ searchspace="darts"
 
 run_names=(
   "darts_we_vanilla"
-  "darts_ws_vanilla"
-  "darts_we_lora_rank_1"
-  "darts_ws_lora_rank_1"
-  "darts_we_lora_alt"
-  "darts_ws_lora_alt"
-  "drnas_we_vanilla"
-  "drnas_ws_vanilla"
-  "drnas_we_lora_rank_1"
-  "drnas_ws_lora_rank_1"
-  "drnas_we_lora_alt"
-  "drnas_ws_lora_alt"
+  # "darts_ws_vanilla"
+  # "darts_we_lora_rank_1"
+  # "darts_ws_lora_rank_1"
+  # "darts_we_lora_alt"
+  # "darts_ws_lora_alt"
+  # "drnas_we_vanilla"
+  # "drnas_ws_vanilla"
+  # "drnas_we_lora_rank_1"
+  # "drnas_ws_lora_rank_1"
+  # "drnas_we_lora_alt"
+  # "drnas_ws_lora_alt"
 )
 
 # Iterate over the lists and call the existing script
