9d07eaa2ee5860ffc9ea1acf793e63390ecbea9f
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-06-04 23:15:14 +0200

    chore: update partition

--- scripts/jobs/train_discrete_genotype.sh
+++ scripts/jobs/train_discrete_genotype.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -p alldlc_gpu-rtx2080 #alldlc_gpu-rtx2080 #relea_gpu-rtx2080 #mlhiwidlc_gpu-rtx2080 # partition (queue)
+#SBATCH -p mlhiwidlc_gpu-rtx2080 #alldlc_gpu-rtx2080 #relea_gpu-rtx2080 #mlhiwidlc_gpu-rtx2080 # partition (queue)
 #SBATCH -o logs/%x.%N.%j.out # STDOUT
 #SBATCH -e logs/%x.%N.%j.err # STDERR
 #SBATCH -a 9001-9003 # array size
