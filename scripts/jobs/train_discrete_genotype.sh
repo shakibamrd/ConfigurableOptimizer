a04fb9ca078c9ab50038b8d2d3a204c3dec40a9a
Arjun <arjun.krishnakumar.mec@gmail.com>
2024-06-04 08:02:19 +0200

    chore: change seeds

--- scripts/jobs/train_discrete_genotype.sh
+++ scripts/jobs/train_discrete_genotype.sh
@@ -2,7 +2,7 @@
 #SBATCH -p ml_gpu-rtx2080 #alldlc_gpu-rtx2080 #relea_gpu-rtx2080 #mlhiwidlc_gpu-rtx2080 # partition (queue)
 #SBATCH -o logs/%x.%N.%j.out # STDOUT
 #SBATCH -e logs/%x.%N.%j.err # STDERR
-#SBATCH -a 9001-9003 # array size
+#SBATCH -a 4-6 # array size
 #SBATCH --time 3-00:00:00 # time (D-HH:MM)
 #SBATCH --cpus-per-task 2
 #SBATCH -J TRAIN_GENOTYPE # sets the job name.
