ba4c87222dafad49947d7c564654dc63a0df518d
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2024-06-04 22:55:23 +0200

    Update submit_train_genotype_jobs.sh

--- scripts/jobs/submit_train_genotype_jobs.sh
+++ scripts/jobs/submit_train_genotype_jobs.sh
@@ -2,34 +2,6 @@
 
 searchspace="darts"
 
-# write a list of genotypes to train
-# change run name
-# run_names=(
-#     "darts_we_vanilla"
-#     "darts_ws_vanilla"
-#     "drnas_we_vanilla"
-#     "drnas_ws_vanilla"
-#     "darts_we_lora_rank_1"
-#     "darts_ws_lora_rank_1"
-#     "drnas_we_lora_rank_1"
-#     "drnas_ws_lora_rank_1"
-# )
-
-# run_names=(
-  # "darts_we_vanilla"
-  # "darts_ws_vanilla"
-  # "darts_we_lora_rank_1"
-  # "darts_ws_lora_rank_1"
-  # "darts_we_lora_alt"
-  # "darts_ws_lora_alt"
-  # "drnas_we_vanilla"
-  # "drnas_ws_vanilla"
-  # "drnas_we_lora_rank_1"
-  # "drnas_ws_lora_rank_1"
-  # "drnas_we_lora_alt"
-  # "drnas_ws_lora_alt"
-# )
-
 run_names=(
   "darts_we_lora_epoch_50"
   "darts_ws_lora_epoch_50"
@@ -37,9 +9,6 @@ run_names=(
   "darts_ws_vanilla_epoch_50"
 )
 
-
-
-# Iterate over the lists and call the existing script
 for i in "${!run_names[@]}"; do
   run_name=${run_names[$i]}
   sbatch -J $run_name scripts/jobs/train_discrete_genotype.sh "$searchspace" "$run_name"
