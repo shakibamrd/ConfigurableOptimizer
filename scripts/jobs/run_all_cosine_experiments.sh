7d3b1f8997a69d426b694dd9650171ddd10bb6d1
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2024-08-27 19:18:36 +0200

    fix: do not run code on login node

--- scripts/jobs/run_all_cosine_experiments.sh
+++ scripts/jobs/run_all_cosine_experiments.sh
@@ -9,10 +9,8 @@ for space in "${spaces[@]}"; do
     for sampler in "${samplers[@]}"; do
         for t0 in "${t0s[@]}"; do
             for tmult in "${tmults[@]}"; do
-                # echo scripts/jobs/submit_cosine_annealing_experiment.sh $space cifar10 50 $sampler $t0 $tmult
-                echo sbatch -J $space-$sampler-t0$t0-tmult$tmult-cf10-50e scripts/jobs/submit_cosine_annealing_experiment.sh $space cifar10 50 $sampler $t0 $tmult
-                python scripts/baselines/run_search.py --sampler $sampler --searchspace $space --dataset cifar10 --search_epochs 50 --cosine_anneal_restarts_T0 $t0 --cosine_anneal_restarts_Tmult $tmult --seed 9001
-
+                echo scripts/jobs/submit_cosine_annealing_experiment.sh $space cifar10 50 $sampler $t0 $tmult
+                sbatch -J $space-$sampler-t0$t0-tmult$tmult-cf10-50e scripts/jobs/submit_cosine_annealing_experiment.sh $space cifar10 50 $sampler $t0 $tmult
             done
         done
     done
