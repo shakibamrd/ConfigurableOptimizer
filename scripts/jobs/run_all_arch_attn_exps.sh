9c6dbccccbfce05e900c1654de6b5d8f124f05e9
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2024-09-08 22:16:04 +0200

    experiment: add new experiment scripts

---
+++ scripts/jobs/run_all_arch_attn_exps.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+spaces=("darts" "nb201")
+samplers=("darts" "drnas" "gdas" "reinmax")
+
+for space in "${spaces[@]}"; do
+    for sampler in "${samplers[@]}"; do
+        if [ $sampler == "darts" ]; then
+            epochs=50
+        elif [ $sampler == "drnas" ]; then
+            epochs=100
+        elif [ $sampler == "gdas" ]; then
+            epochs=250
+        elif [ $sampler == "reinmax" ]; then
+            epochs=250
+        else
+            echo "Unknown sampler"
+            exit 1
+        fi
+
+        for k in "${ks[@]}"; do
+            echo scripts/jobs/submit_arch_attn_exp.sh $space $sampler $epochs
+            sbatch -J ${sampler}-${space}-k${k}-warm_epochs${warm_epochs} scripts/jobs/submit_arch_attn_partial_connection_exp.sh $space $sampler $epochs
+        done
+
+    done
+done
+
---
+++ scripts/jobs/submit_arch_attn_exp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -p alldlc_gpu-rtx2080
+#SBATCH -o logs/%x.%N.%j.out # STDOUT
+#SBATCH -e logs/%x.%N.%j.err # STDERR
+#SBATCH -a 1-4 # array size
+#SBATCH -J ConfoptExperiment # sets the job name. If not
+echo "Workingdir: $PWD";
+echo "Started at $(date)";
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CUPS_PER_NODE gpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
+
+start=`date +%s`
+
+source ~/.bashrc
+conda activate confopt
+
+searchspace=$1
+sampler=$2
+epochs=$3
+
+python scripts/experiments/run_experiment.py \
+        --searchspace $searchspace \
+        --dataset "cifar10" \
+        --sampler $sampler \
+        --epochs $epochs \
+        --seed $SLURM_ARRAY_TASK_ID \
+        --arch-attention-enabled \
+        --project-name iclr-experiments \
+        --rq arch-attention \
+        --comment "for non-darts searchspaces, the trainer config is wrong"
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo Runtime: $runtime
