7fe73bf65e03f54c4c8707afe5ec4417153f5b3d
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2024-09-19 00:09:47 +0200

    chore: add script to run OLES threshold ablation

---
+++ scripts/jobs/run_darts_darts_oles_threshold_ablation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+thresholds=(0.3 0.4 0.5)
+
+searchspace=darts
+dataset=cifar10
+sampler=darts
+epochs=50
+frequency=20
+meta_info="'OLES Threshold Ablation'"
+comments="'None'"
+
+for threshold in "${thresholds[@]}"; do
+    exp_name=${searchspace}-${dataset}-${sampler}-epochs${epochs}-threshold${threshold}
+    echo $exp_name scripts/jobs/submit_oles_experiment_job.sh $searchspace $dataset $sampler $epochs $frequency $threshold $meta_info $comments
+    # sbatch -J $exp_name scripts/jobs/submit_oles_experiment_job.sh $searchspace $dataset $sampler $epochs $frequency $threshold $meta_info $comments
+done
+
---
+++ scripts/jobs/submit_oles_experiment_job.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -p alldlc_gpu-rtx2080
+#SBATCH -o logs/%x.%N.%j.out # STDOUT
+#SBATCH -e logs/%x.%N.%j.err # STDERR
+#SBATCH -a 0-3 # array size
+#SBATCH --cpus-per-task=8 # Number of CPU cores per task
+#SBATCH -J DARTS-OLES-Experiment # sets the job name. If not
+echo "Workingdir: $PWD";
+echo "Started at $(date)";
+echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CUPS_PER_NODE gpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
+
+start=`date +%s`
+
+source ~/.bashrc
+conda activate confopt
+
+searchspace=$1
+dataset=$2
+sampler=$3
+epochs=$4
+oles_frequency=$5
+oles_threshold=$6
+meta_info=$7
+comments=$8
+
+python scripts/experiments/run_experiment.py \
+        --searchspace $searchspace \
+        --dataset $dataset \
+        --sampler $sampler \
+        --epochs $epochs \
+        --oles \
+        --oles-freq $oles_frequency \
+        --oles-threshold $oles_threshold \
+        --seed $SLURM_ARRAY_TASK_ID \
+        --project-name iclr-experiments \
+        --meta-info $meta_info \
+        --comments $comments
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo Runtime: $runtime
