22535666a40e0f3ed1959923116b4d5743d294b8
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2024-09-20 15:21:16 +0200

    experiment: add scripts for DARTS with pruning experiments

---
+++ scripts/jobs/run_darts_darts_prune_rank_ablation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ranks=(1 2 4 8)
+
+searchspace=darts
+dataset=cifar10
+sampler=darts
+epochs=50
+lora_warmup=16
+meta_info="'DARTS-Prune-LoRA-Rank-Ablation'"
+comments="'None'"
+
+for rank in "${ranks[@]}"; do
+    exp_name=${searchspace}-${dataset}-${sampler}-epochs${epochs}-rank${rank}-warm${lora_warmup}
+    echo $exp_name scripts/jobs/submit_lora_prune_experiment_job.sh $searchspace $dataset $sampler $epochs $rank $lora_warmup $meta_info $comments
+    sbatch -J $exp_name scripts/jobs/submit_lora_prune_experiment_job.sh $searchspace $dataset $sampler $epochs $rank $lora_warmup $meta_info $comments
+done
+
