04ed989f9558ba9bb9a15df6e426d5c9f112cae2
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2024-09-20 23:09:22 +0200

    chore: update wandb utils

--- scripts/analysis/wandb_utils.py
+++ scripts/analysis/wandb_utils.py
@@ -97,3 +97,110 @@ def make_wandb_filters(
 def get_columns(df: pd.DataFrame, str_filters: list[str]) -> list:
     columns = [c for c in df.columns if all(f in c for f in str_filters)]
     return sorted(columns)
+
+
+cell_type_to_idx = {"normal": 0, "reduce": 1}
+
+
+def get_df_with_columns(df: pd.DataFrame, str_filters: list[str]) -> pd.DataFrame:
+    columns = get_columns(df, str_filters)
+    return df[columns]
+
+
+def get_normalized_arch_values_by_edge(
+    df: pd.DataFrame, cell_type: str, edge_idx: int
+) -> pd.DataFrame:
+    str_filters = [
+        "arch_values",
+        f"alpha_{cell_type_to_idx[cell_type]}",
+        f"edge_{edge_idx}",
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_normalized_arch_values_by_op(
+    df: pd.DataFrame, cell_type: str, op_idx: int
+) -> pd.DataFrame:
+    str_filters = [
+        "arch_values",
+        f"alpha_{cell_type_to_idx[cell_type]}",
+        f"op_{op_idx}",
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_cell_grad_norm(df: pd.DataFrame, cell_idx: int) -> pd.DataFrame:
+    str_filters = [
+        "gradient_stats/",
+        f"cell_{cell_idx}_grad_norm",
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_arch_param_grad_norm(df: pd.DataFrame, cell_type: str) -> pd.DataFrame:
+    str_filters = [
+        "gradient_stats/",
+        f"arch_param_{cell_type_to_idx[cell_type]}_grad_norm",
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_arch_param_grad_norm_by_edge(
+    df: pd.DataFrame, cell_type: str, edge_idx: int
+) -> pd.DataFrame:
+    str_filters = [
+        "gradient_stats/",
+        f"arch_param_{cell_type_to_idx[cell_type]}_row_{edge_idx}_grad_norm",
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_skip_connections(df: pd.DataFrame, cell_type: str) -> pd.DataFrame:
+    str_filters = [
+        f"skip_connections/{cell_type}",
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_mean_gradient_matching_score(df: pd.DataFrame) -> pd.DataFrame:
+    str_filters = [
+        "gm_scores/mean_gm",
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_benchmark_test_acc(df: pd.DataFrame) -> pd.DataFrame:
+    str_filters = [
+        "benchmark/test_top1",
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_layer_alignment_scores_all_cells(
+    df: pd.DataFrame, cell_type: str
+) -> pd.DataFrame:
+    str_filters = [
+        "layer_alignment_scores/mean/",
+        cell_type,
+    ]
+
+    return get_df_with_columns(df, str_filters)
+
+
+def get_layer_alignment_scores_first_and_last_cells(
+    df: pd.DataFrame, cell_type: str
+) -> pd.DataFrame:
+    str_filters = [
+        "layer_alignment_scores/first_last/",
+        cell_type,
+    ]
+
+    return get_df_with_columns(df, str_filters)
