847c33769b6c9c5ec928552337cd0a14a1805808
abhash-er <abhashk2@outlook.com>
2024-09-23 22:46:31 +0200

    fix(get_darts_best_genotype.sh): replace bash with sbatch

--- scripts/jobs/get_darts_best_genotype.sh
+++ scripts/jobs/get_darts_best_genotype.sh
@@ -5,5 +5,5 @@ genotype_exps=("test")
 
 for exp in "${genotype_exps[@]}"; do
     genotype_folder="scripts/genotypes/${exp}"
-    bash scripts/jobs/submit_darts_best_genotype_jobs.sh $genotype_folder
+    sbatch -J DARTS-Genotype-100-epochs-${exp} scripts/jobs/submit_darts_best_genotype_jobs.sh $genotype_folder
 done 
