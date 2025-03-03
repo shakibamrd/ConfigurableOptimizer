3625b319d91d7686f3288cf443993c7324888125
Arjun Krishnakumar <arjun.krishnakumar.mec@gmail.com>
2025-03-03 15:38:06 +0100

    chore: add new search spaces to init

--- src/confopt/searchspace/__init__.py
+++ src/confopt/searchspace/__init__.py
@@ -2,7 +2,9 @@ from .baby_darts.supernet import BabyDARTSSearchSpace
 from .common.base_search import ArchAttentionSupport, SearchSpace
 from .darts.core import DARTSImageNetModel, DARTSModel  # type: ignore
 from .darts.core.genotypes import DARTSGenotype
-from .darts.supernet import DARTSSearchSpace  # type: ignore
+from .darts.supernet import (  # type: ignore
+    DARTSSearchSpace,
+)
 from .nb1shot1.supernet import NASBench1Shot1SearchSpace  # type: ignore
 from .nb201.core import NASBench201Model  # type: ignore
 from .nb201.core.genotypes import Structure as NAS201Genotype
