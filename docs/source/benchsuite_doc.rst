f4991cfad8dc408cd8e49c233938e09408f21b3c
shakibamrd <shakibamrd@users.noreply.github.com>
2025-04-17 10:45:58 +0000

    deploy: de71c4ab049d03ca8606f66dd3f78353ce6eccbe

--- docs/source/benchsuite_doc.rst
+++
@@ -1,5 +0,0 @@
-Bench Suite Documentation
-=========================
-
-.. include:: ./benchsuite_doc.md
-   :parser: myst_parser.sphinx_
--- src/confopt/searchspace/darts/core/__init__.py
+++
@@ -1,9 +0,0 @@
-from .model import NetworkCIFAR as DARTSModel
-from .model import NetworkImageNet as DARTSImageNetModel
-from .model_search import Network as DARTSSearchModel
-
-__all__ = [
-    "DARTSSearchModel",
-    "DARTSModel",
-    "DARTSImageNetModel",
-]
